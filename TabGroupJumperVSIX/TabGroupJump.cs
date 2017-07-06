1168da0b8cee074359e218e6a208a0eef484f92f
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2017-07-06 17:16:52 -0500

    Refactor the way that commands work
    Next/Previous now work like Left/Right used to work
    Right/Left now do not navigate to groups above/below
    Up/Down now do not navigate to groups left/right

--- TabGroupJumperVSIX/TabGroupJump.cs
+++ TabGroupJumperVSIX/TabGroupJump.cs
@@ -12,6 +12,8 @@ using Microsoft.VisualStudio.Shell.Interop;
 using EnvDTE80;
 using EnvDTE;
 using System.Collections.Generic;
+using System.Linq;
+using Windows = EnvDTE.Windows;
 
 namespace TabGroupJumperVSIX
 {
@@ -27,11 +29,20 @@ namespace TabGroupJumperVSIX
         public const int CommandIdJumpRight = 0x0101;
         public const int CommandIdJumpUp = 0x0102;
         public const int CommandIdJumpDown = 0x0103;
+        public const int CommandIdJumpPrevious = 0x0104;
+        public const int CommandIdJumpNext = 0x0105;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
-        public static readonly Guid CommandSet = new Guid("67e760eb-74c3-46f2-9b85-c7af2d351428");
+        public enum Mode
+        {
+            Horizontal,
+            Vertical,
+            Navigation
+        }
+
+    /// <summary>
+    /// Command menu group (command set GUID).
+    /// </summary>
+    public static readonly Guid CommandSet = new Guid("67e760eb-74c3-46f2-9b85-c7af2d351428");
 
         /// <summary>
         /// VS Package that provides this command, not null.
@@ -57,13 +68,17 @@ namespace TabGroupJumperVSIX
             {
                 // Add the jump commands to the handler
                 commandService.AddCommand(
-                    new MenuCommand(this.MenuItemCallback, new CommandID(CommandSet, CommandIdJumpLeft)));
+                    new MenuCommand(MenuItemCallback_Horizontal, new CommandID(CommandSet, CommandIdJumpLeft)));
                 commandService.AddCommand(
-                    new MenuCommand(this.MenuItemCallback, new CommandID(CommandSet, CommandIdJumpRight)));
+                    new MenuCommand(MenuItemCallback_Horizontal, new CommandID(CommandSet, CommandIdJumpRight)));
                 commandService.AddCommand(
-                    new MenuCommand(this.MenuItemCallback, new CommandID(CommandSet, CommandIdJumpUp)));
+                    new MenuCommand(MenuItemCallback_Vertical, new CommandID(CommandSet, CommandIdJumpUp)));
                 commandService.AddCommand(
-                    new MenuCommand(this.MenuItemCallback, new CommandID(CommandSet, CommandIdJumpDown)));
+                    new MenuCommand(MenuItemCallback_Vertical, new CommandID(CommandSet, CommandIdJumpDown)));
+                commandService.AddCommand(
+                    new MenuCommand(MenuItemCallback_PreviousNext, new CommandID(CommandSet, CommandIdJumpPrevious)));
+                commandService.AddCommand(
+                    new MenuCommand(MenuItemCallback_PreviousNext, new CommandID(CommandSet, CommandIdJumpNext)));
             }
         }
 
@@ -96,59 +111,110 @@ namespace TabGroupJumperVSIX
             Instance = new TabGroupJump(package);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private void MenuItemCallback(object sender, EventArgs e)
+        private IEnumerable<Window> GetValidDocuments(EnvDTE.Windows dteWindows)
         {
-            DTE2 dte = (DTE2)this.ServiceProvider.GetService(typeof(DTE));
-            int commandId = ((MenuCommand)sender).CommandID.ID;
-            bool horizontal = commandId == CommandIdJumpLeft || commandId == CommandIdJumpRight;
+            return dteWindows.Cast<Window>()
+                             .Where(w => w.Kind == "Document")
+                             .Where(w => w.Top != 0 || w.Left != 0)
+                             .OrderBy(w => w.Left)
+                             .ThenBy(w => w.Top);
+        }
 
-            // Documents with a "left" or "top" value > 0 are the focused ones in each group, 
-            // so we only need to collect those
-            List<EnvDTE.Window> topLevel = new List<EnvDTE.Window>();
-            foreach (EnvDTE.Window w in dte.Windows)
-            {
-                if (w.Kind == "Document" && (horizontal && w.Left > 0 || w.Top > 0))
-                    topLevel.Add(w);
-            }
-
-            if (topLevel.Count == 0)
-                return;
-
-            if (horizontal)
-                topLevel.Sort((a, b) => a.Left < b.Left ? -1 : 1);
-            else
-                topLevel.Sort((a, b) => a.Top < b.Top ? -1 : 1);
-
-            // find the index of the active document
-            var activeDoc = dte.ActiveDocument;
+        private int FindIndex(List<Window> windows, Document document)
+        {
+            var activeDoc = document;
             int activeIdx = 0;
-            for (int i = 0; i < topLevel.Count; ++i)
+            for (int i = 0; i < windows.Count; ++i)
             {
-                if (topLevel[i].Document == activeDoc)
+                if (windows[i].Document == activeDoc)
                 {
                     activeIdx = i;
                     break;
                 }
             }
 
-            // set the new active document
-            if (horizontal)
-            {
-                activeIdx += commandId == CommandIdJumpLeft ? -1 : 1;
-            }
-            else
-            {
-                activeIdx += commandId == CommandIdJumpUp ? -1 : 1;
-            }
+            return activeIdx;
+        }
 
-            activeIdx = (activeIdx < 0 ? activeIdx + topLevel.Count : activeIdx) % topLevel.Count;
+        private void MenuItemCallback_Horizontal(object sender, EventArgs e)
+        {
+            DTE2 dte = (DTE2)this.ServiceProvider.GetService(typeof(DTE));
+            int commandId = ((MenuCommand)sender).CommandID.ID;
+
+            // Documents with a "left" or "top" = 0 are the non-focused ones in each group, so don't
+            // collect those. 
+            var topLevel = GetValidDocuments(dte.Windows)
+                .OrderBy(w => w.Left)
+                .ThenBy(w => w.Top)
+                .Where(w => w.Document == dte.ActiveDocument 
+                       || w.Left != dte.ActiveDocument.ActiveWindow.Left)
+                .ToList();
+
+            if (topLevel.Count == 0)
+                return;
+
+            // find the index of the active document
+            var activeIdx = FindIndex(topLevel, dte.ActiveDocument);
+
+            // set the new active document
+            activeIdx += commandId == CommandIdJumpLeft ? -1 : 1;
+
+            activeIdx = Clamp(activeIdx, topLevel.Count);
+            topLevel[activeIdx].Activate();
+        }
+
+        private void MenuItemCallback_Vertical(object sender, EventArgs e)
+        {
+            DTE2 dte = (DTE2)this.ServiceProvider.GetService(typeof(DTE));
+            int commandId = ((MenuCommand)sender).CommandID.ID;
+
+            var topLevel = GetValidDocuments(dte.Windows)
+                .Where(w => w.Document == dte.ActiveDocument
+                            || w.Left == dte.ActiveDocument.ActiveWindow.Left)
+                .OrderBy(w => w.Top)
+                .ThenBy(w => w.Left)
+                .ToList();
+
+            if (topLevel.Count == 0)
+                return;
+
+            // find the index of the active document
+            var activeIdx = FindIndex(topLevel, dte.ActiveDocument);
+
+            // set the new active document
+            activeIdx += commandId == CommandIdJumpDown ? -1 : 1;
+
+            activeIdx = Clamp(activeIdx, topLevel.Count);
+            topLevel[activeIdx].Activate();
+        }
+
+        private static int Clamp(int activeIdx, int count)
+        {
+            return (activeIdx < 0 ? activeIdx + count : activeIdx) % count;
+        }
+
+        private void MenuItemCallback_PreviousNext(object sender, EventArgs e)
+        {
+            DTE2 dte = (DTE2)this.ServiceProvider.GetService(typeof(DTE));
+            int commandId = ((MenuCommand)sender).CommandID.ID;
+
+            // Documents with a "left" or "top" = 0 are the non-focused ones in each group, so don't
+            // collect those. 
+            var topLevel = GetValidDocuments(dte.Windows)
+                .OrderBy(w => w.Left)
+                .ThenBy(w => w.Top)
+                .ToList();
+
+            if (topLevel.Count == 0)
+                return;
+
+            // find the index of the active document
+            var activeIdx = FindIndex(topLevel, dte.ActiveDocument);
+
+            // set the new active document
+            activeIdx += commandId == CommandIdJumpPrevious ? -1 : 1;
+
+            activeIdx = Clamp(activeIdx, topLevel.Count);
             topLevel[activeIdx].Activate();
         }
     }
